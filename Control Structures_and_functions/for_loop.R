bc4bac1a4ecd5d8056487d909fc2aca3f877008b
Zakir Ullah <Zakirullah98>
2025-09-22 08:06:06 -0700

    Initial commit

---
+++ Control Structures_and_functions/for_loop.R
@@ -0,0 +1,5 @@
+#x<-0
+for(i in 1:10) {      # Head of for-loop
+  #x <- x + 1  # Body of for-loop
+  print(i)
+}
---
+++ Control Structures_and_functions/switch_case.R
@@ -0,0 +1,11 @@
+a <- 8
+x<-switch(a,
+       "3"="this is the third case in switch",
+       "1"="this is the first case in switch",
+       "2"="this is the second case in switch",
+       "4"="this is the fourth case in switch",
+       "5"="this is the fifth case in switch",
+       "abc"
+)
+
+print(x)
