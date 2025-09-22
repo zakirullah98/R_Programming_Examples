f9260887b1868104732d17de66217e4f2472fe2c
Zakir Ullah <Zakirullah98>
2025-09-22 08:17:15 -0700

    Initial commit

---
+++ Control_Structures_and_functions/if_else.R
@@ -0,0 +1,9 @@
+a <- 5
+b <- 6
+if(a>b){
+  print("a is greater than b")
+}else
+  
+{
+  print("b is greater than a")
+}
---
+++ Control_Structures_and_functions/while_loop.R
@@ -0,0 +1,7 @@
+x <- 0
+while(x < 2) {        # Head of while-loop
+  
+  x <- x + 1 
+  # Body of while-loop
+  print(x)
+}
